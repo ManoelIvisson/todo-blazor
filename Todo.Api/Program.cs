7b3fcc354b669a421d7bd5ec605ae16250450ffb
Manoel Ívisson <121980133+ManoelIvisson@users.noreply.github.com>
2026-03-09 16:48:13 +0000

    Criando as rotas de Delete e Put

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -36,4 +36,28 @@ app.MapPost("todos", async (TodoItem todo, AppDbContext db) =>
     return Results.Created($"/todos/{todo.Id}", todo);
 });
 
+app.MapPut("/todos/{id}", async (int id, TodoItem inputTodo, AppDbContext db) => 
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Title = inputTodo.Title;
+    todo.Completed = inputTodo.Completed;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/todos/{id}", async (int id, AppDbContext db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    db.Todos.Remove(todo);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
 app.Run();
